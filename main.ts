d045b2e5cf327fe299c1cd2da6d02be341cb2af2
蜜蜂QWQ <158593086+FrecklyComb1728@users.noreply.github.com>
2025-06-07 03:43:30 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const TARGET_BASE = "https://testingcf.jsdelivr.net/gh/FrecklyComb1728/image-cdn@master/";
+const TARGET_BASE = "https://cdn.statically.io/gh/FrecklyComb1728/image-cdn@master/";
 const FAVICON_PATH = "./favicon.ico";
 const CACHE_MAX_AGE = 5184000; // 24小时缓存（单位：秒）
 
